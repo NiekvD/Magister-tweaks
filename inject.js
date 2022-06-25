441ff61688dfebc3a2241e53804f1547fff4fb86
Niek <94279766+NiekvD@users.noreply.github.com>
2022-06-25 14:29:35 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,3 +1,23 @@
 window.onload = function() {
-    document.body.parentNode.style.filter = 'invert(1) hue-rotate(180deg)';
+    f1();
+}
+
+
+function resolveAfter2Seconds(x) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(x);
+        }, 500);
+    });
+}
+
+async function f1() {
+    var x = await resolveAfter2Seconds(10);
+    flip_screen();
+}
+
+function flip_screen() {
+    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+    let media = document.querySelectorAll("img, picture, video");
+    media[0].style.filter = "invert(1) hue-rotate(180deg)";
 }
